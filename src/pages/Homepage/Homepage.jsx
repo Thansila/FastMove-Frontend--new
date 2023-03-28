af919426dd6bb2f043e02722e6abf059ab1d2bce
Dasuni <dasunichethanadc@gmail.com>
2023-03-28 07:32:08 +0530

    Add css to homepage and loginpage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -27,10 +27,26 @@ export default function Homepage(){
         </ul>
 
     </nav> 
+{/* <Link to="/SignInchar"><button type="button" onClick={""} class="btn btn-primary ml-1">Get Started</button></Link> */}
+<div className='screen'>
+<div class="split left">
+  <div class="centered">
+    <h2>Book smart-Travel safe</h2>
+    <p>FastMove: smart Bus Pass booking application to book your ticket smart and conveniently</p>
+    <p>Press Get Started to Sign in and enjoy</p>
 
+    <Link to="/SignInchar"><div className='btn'><button type="button" onClick={""} class="btn btn-primary ml-1">Get Started</button></div></Link>
+    
+  </div>
+</div>
 
-<Link to="/SignInchar"><button type="button" onClick={""} class="btn btn-primary ml-1">Get Started</button></Link>
+<div class="split right">
+  <div class="centered">
+    <p>/Logo/</p>
+  </div>
+</div> 
+</div>
+</div>
 
-        </div>
     )
 }
